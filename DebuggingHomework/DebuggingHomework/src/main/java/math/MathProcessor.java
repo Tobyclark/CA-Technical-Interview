20583294af0cb258ef8145c64f3d15a598a0eac1
Tobyclark <65095945+Tobyclark@users.noreply.github.com>
2025-07-14 16:29:31 -0700

    fix concurrency bug

--- DebuggingHomework/DebuggingHomework/src/main/java/math/MathProcessor.java
+++ DebuggingHomework/DebuggingHomework/src/main/java/math/MathProcessor.java
@@ -15,31 +15,31 @@ public class MathProcessor {
         }
     }
 
-    private double value = 0;
-    private final List<MathOperation> operations = new ArrayList<>();
+    private ThreadLocal<Double> value = ThreadLocal.withInitial(() -> 0.0);
+    private final ThreadLocal<List<MathOperation>> operations = ThreadLocal.withInitial(ArrayList::new);
 
     public static MathProcessor getInstance() {
         return instance;
     }
 
     public MathProcessor start(double value) {
-        this.value = value;
-        this.operations.clear();
+        this.value.set(value);
+        this.operations.get().clear();
         return this;
     }
 
     public MathProcessor addOperator(double value, MathOperator operator) {
         if (operator == null) throw new IllegalArgumentException("Operator cannot be null");
 
-        operations.add(new MathOperation(value, operator));
+        operations.get().add(new MathOperation(value, operator));
         return this;
     }
 
     public double calculate() {
-        double result = this.value;
-        for(int i = 0; i < this.operations.size(); i++) {
-            System.out.printf("Applying operation %d of %d to number %f\n", (i + 1), this.operations.size(), result);
-            result = operations.get(i).calculate(result);
+        double result = this.value.get();
+        for(int i = 0; i < this.operations.get().size(); i++) {
+            System.out.printf("Applying operation %d of %d to number %f\n", (i + 1), this.operations.get().size(), result);
+            result = operations.get().get(i).calculate(result);
         }
         return result;
     }
